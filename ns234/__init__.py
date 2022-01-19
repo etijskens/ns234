d39e5e822fb4c59a7fb3fed8a0f18719e7fd898b
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2022-01-19 18:30:28 +0100

    coordinate conversion from png file

--- ns234/__init__.py
+++ ns234/__init__.py
@@ -9,14 +9,10 @@ Top-level package for ns234.
 
 __version__ = "0.0.0"
 
+import numpy as np
 
-def hello(who='world'):
-    """'Hello world' method.
-
-    :param str who: whom to say hello to
-    :returns: a string
-    """
-    result = "Hello " + who
-    return result
-
-# Your code here...
+def distance(p,q):
+    dx = q[0] - p[0]
+    dy = q[1] - p[1]
+    d = np.sqrt(dx*dx + dy*dy)
+    return d
--- tests/ns234/test_ns234.py
+++ tests/ns234/test_ns234.py
@@ -4,17 +4,52 @@
 
 import ns234
 
+import numpy as np
 
-def test_hello_noargs():
-    """Test for ns234.hello()."""
-    s = ns234.hello()
-    assert s == "Hello world"
+def test_coordinates():
+    """Test converting coordinates.
+    png_coordinates obtained by opening kadaster-ns234.png in gimp, and reading of the pixel coordinates.
+    Pixel coordinates have the origin in the upper left corner, so the X-axis is correct, but the Y-axis is
+    in the wrong direction.
+    """
+    png_coordinates = { 
+        'A' : (316,544),
+        'B' : (527,544),
+        '0' : (633,468),
+        '1' : (533,484),
+        '2' : (444,461),
+        '3' : (453,431),
+        '4' : (398,416),
+        '5' : (412,368),
+        '6' : (426,319),
+        '7' : (534,350),
+        '8' : (635,353),
+        '9' : (635,406),
+        '10' : (534,405),
+        '11' : ( 84,372),
+        '12a': (136,149),
+        '12b': (145,122),
+        # '13' : (,),
+        # '14' : (,),
+        # '15' : (,),
+        # '16' : (,),
+        # '17' : (,),
+        # '18' : (,),
+        # '19' : (,),
+        # '20' : (,),
+        # '21' : (,),
+        # '22' : (,),
+    }
+    coordinates = {}
+    scale_factor = 20 / ns234.distance(png_coordinates['A'],png_coordinates['B'])
+    print(f"{scale_factor=}")
+    digits = 2
+    for id,xy in png_coordinates.items():
+        x = round( scale_factor * xy[0], digits)
+        y = round(-scale_factor * xy[1], digits)
+        coordinates[id] = (x,y)
+        print(f"{id=} = ({x},{y})")
 
-
-def test_hello_me():
-    """Test for ns234.hello('me')."""
-    s = ns234.hello('me')
-    assert s == "Hello me"
 
 
 # ==============================================================================
@@ -24,7 +59,7 @@ def test_hello_me():
 # that the source directory is on the path
 # ==============================================================================
 if __name__ == "__main__":
-    the_test_you_want_to_debug = test_hello_noargs
+    the_test_you_want_to_debug = test_coordinates
 
     print("__main__ running", the_test_you_want_to_debug)
     the_test_you_want_to_debug()
