2dc920267b3c8726067d69e05e7650e1885e18b4
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2022-01-27 11:25:48 +0100

    draw kadaster plan

--- tests/ns234/test_ns234.py
+++ tests/ns234/test_ns234.py
@@ -5,6 +5,7 @@
 import ns234
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def test_coordinates():
     """Test converting coordinates.
@@ -15,7 +16,7 @@ def test_coordinates():
     png_coordinates = { 
         'A' : (316,544),
         'B' : (527,544),
-        '0' : (633,468),
+        '0' : (633,486),
         '1' : (533,484),
         '2' : (444,461),
         '3' : (453,431),
@@ -29,28 +30,40 @@ def test_coordinates():
         '11' : ( 84,372),
         '12a': (136,149),
         '12b': (145,122),
-        # '13' : (,),
-        # '14' : (,),
-        # '15' : (,),
-        # '16' : (,),
-        # '17' : (,),
-        # '18' : (,),
-        # '19' : (,),
-        # '20' : (,),
-        # '21' : (,),
-        # '22' : (,),
+        '13' : (377,305),
+        '14' : (476,334),
+        '15' : (476,275),
+        '16' : (637,278),
+        '17' : (117,273),
+        '18' : (139,173),
+        '19' : (187,183),
+        '20' : (185,197),
+        '21' : (193,198),
+        '22' : (174,285),
     }
     coordinates = {}
     scale_factor = 20 / ns234.distance(png_coordinates['A'],png_coordinates['B'])
     print(f"{scale_factor=}")
     digits = 2
+    origin = png_coordinates['5']
     for id,xy in png_coordinates.items():
-        x = round( scale_factor * xy[0], digits)
-        y = round(-scale_factor * xy[1], digits)
+        x = round( scale_factor * (xy[0] - origin[0]), digits)
+        y = round(-scale_factor * (xy[1] - origin[1]), digits)
         coordinates[id] = (x,y)
         print(f"{id=} = ({x},{y})")
-
-
+    lines = [['0','1','2','11','12a','12b','13','6','7','8','9','0']
+            ,['2','3','4','5','6']
+            ,['5','10','9']
+            ,['14','15','16','8']
+            ,['17','18','19','20','21','22','17']
+            ]
+    plt.figure()
+    for line in lines:
+        for i1 in range(1,len(line)):
+            xy0 = coordinates[line[i1-1]]
+            xy1 = coordinates[line[i1]]
+            plt.plot([xy0[0],xy1[0]], [xy0[1],xy1[1]])
+    plt.show()
 
 # ==============================================================================
 # The code below is for debugging a particular test in eclipse/pydev.
